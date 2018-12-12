ae7ea27f9a73de6e212ad91b83ce03ad6b8793fa
CezHudzi <hudzik.cezary@gmail.com>
2018-12-12 17:53:45 +0100

    Starts threath

--- src/com/cezhudzik/window/Game.java
+++ src/com/cezhudzik/window/Game.java
@@ -10,18 +10,39 @@ public class Game extends Canvas implements Runnable{
 private static final  long serialVersionUID = -11111111111L;
 
 
+    private boolean running = false;
+    private Thread thread;
+
+    //START IS PROPABLY RUN AUTOMATICLY ONCE
+    public synchronized void start()
+    {
+        if(running)
+        {
+            return;
+        }
+
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+
+    }
+
 
     @Override
     public void run() {
+
+
 
     }
 
 
     public static void main(String[] args) {
-        new Window(800,600,"Skoki",new Game());
+
+        Game game = new Game();
+        game.start();
+        new Window(800,600,"Skoki",game);
+
     }
-
-
 
 
 
